dd03ea7edffe14705a362aca32fc3089409cdc8f
Kevin Schlosser <drschlosser@hotmail.com>
2020-04-09 01:59:59 -0600

    GlobalCache completed

--- IRDecoder/ir_GlobalCache.py
+++ IRDecoder/ir_GlobalCache.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-
 # Copyright 2016 Hisham Khalifa
 # Copyright 2017 David Conran
+# Copyright 2020 Kevin Schlosser
 
 # Global Cache IR format sender originally added by Hisham Khalifa
 #   (http:#www.hishamkhalifa.com)
@@ -34,10 +34,10 @@ kGlobalCacheStartIndex = kGlobalCacheRptStartIndex + 1
 #   e.g. sendir,1:1,1,38000,1,1,9,70,9,30,9,... .38000,1,1,9,70,9,30,9,...
 # Ref:
 #   https:#irdb.globalcache.com/Home/Database
-def sendGC(buf, length):
+def sendGC(self, buf, length):
     hz = buf[kGlobalCacheFreqIndex]  # GC frequency is in Hz.
-    enableIROut(hz)
-    periodic_time = calcUSecPeriod(hz, False)
+    self.enable_ir_out(hz)
+    periodic_time = self.calc_usec_period(hz, False)
     emits = min(buf[kGlobalCacheRptIndex], kGlobalCacheMaxRepeat)
     # Repeat
     repeat = 0
@@ -54,10 +54,13 @@ def sendGC(buf, length):
             microseconds = max(buf[offset] * periodic_time, kGlobalCacheMinUsec)
             # These codes start at an odd index (not even as with sendRaw).
             if offset & 1:  # Odd bit.
-                mark(microseconds)
+                self.mark(microseconds)
             else:  # Even bit.
-                space(microseconds)
+                self.space(microseconds)
             offset += 1
         repeat += 1
     # It's possible that we've ended on a mark(), thus ensure the LED is off.
-    ledOff()
+    self.led_off()
+
+
+IRsend.sendGC = sendGC
